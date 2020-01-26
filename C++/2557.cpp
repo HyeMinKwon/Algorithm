f333639aa67d953d0849b512345f42629e7fa505
HyeMinKwon <34715608+HyeMinKwon@users.noreply.github.com>
2020-01-27 01:25:35 +0900

    Create 2557.cpp

---
+++ C++/2557.cpp
@@ -0,0 +1,15 @@
+//비주얼스튜디오로 C를 배우다 보면 
+//여러 예제 소스를 한 플젝에서 관리
+// 그런데 한 project에는 하나의 main함수만 필요
+#include<iostream>
+//using namespace std;이거 쓰지말고
+//std::cout
+//std::cin이렇게 쓰기
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	
+	return 0;
+}
+
+//cin쓰는 법 : std::cin >> 변수명
